96c23b7ffbb9732eb16c813b9d44cae4a03807b8
Rafael Medeiros <rafamedeiros0@gmail.com>
2020-04-14 11:23:12 -0300

    Final class 5

--- public/scripts.js
+++ public/scripts.js
@@ -12,3 +12,32 @@ function onOff(){
         .classList
         .toggle("addScroll")
 }
+
+function checkFields(event){
+    
+    const valuesToCheck = [
+        "title",
+        "category",
+        "image",
+        "description",
+        "link",
+    ]
+
+    conso
+
+    const isEmpty = valuesToCheck.find(function(value){
+        
+        const checkIfIsStrng = typeof event.target[value].value === "string"  //exatamente igual ===
+        const checkIfIsEmpty = !event.target[value].value.trim()
+
+        if(checkIfIsStrng && checkIfIsEmpty){
+            return true
+        }
+    })
+
+    if(isEmpty){
+        event.preventDefault()
+        alert("Por favor, preencha todos os campos")
+    }
+
+}
